1222374a59871f4af244ada31a91b65f2b1ee587
jeric <jericjoshxx@gmail.com>
2025-06-27 11:16:38 +0800

    add

--- frontend/src/components/AdminNavBar.jsx
+++ frontend/src/components/AdminNavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import "../styles/NavBar.css";
+import "../styles/Navbar.css";
 import logo from "../assets/icons/uniblog.svg";
 import profilePic from "../assets/img/profilepic.jpg";
 
